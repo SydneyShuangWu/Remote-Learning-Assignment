9a5c95066d91390e4c36040c06e45c09e0bf7539
SydneyShuangWu <shuangwu0120@gmail.com>
2020-09-13 23:46:34 +0800

    week 1 assignment

---
+++ week1/assignment_week 1.playground/Contents.swift
@@ -0,0 +1,168 @@
+//Basic
+
+//Q1
+let Pi: Double = 3.14
+
+//Q2
+let x: Int = 2
+let y: Int = 6
+let average = (x+y)/2
+
+//Q3
+/*
+ 1) Change the type of x and y to Double instead of Int
+ 2) 10/3 = 3, while 10.0/3.0 = 3.3333333333333335. The former is of type Int, while the latter is of type Double
+ */
+
+//Q4
+var flag: Bool = true
+var inputString: String = "Hello world."
+let bitsInBite: Int = 8
+let averageScore: Double = 86.8
+
+//Q5
+var salary = 22000
+salary + 28000
+
+//Q6
+//==
+
+//Q7
+let c = 10
+let d = 3
+let remain = 10 % 3
+
+//Q8
+//let is used to assign constants, while var is used to assign variables
+
+//Q9
+/*
+ 1. Stay consistent in naming.
+ 2. Name functions or methods for the values they return.
+ 3. Add a verb like get, make, or calculate to function names
+ */
+
+//Q10
+/*
+ Type Inference enables a compiler to deduce the type of a particular expression automatically simply by examining the values users provide.
+ */
+
+//Q11
+// It is not allowed to assign value of type 'String' to type 'Int'.
+
+// Collection
+//Q1
+var myFriends = [String]()
+
+//Q2
+myFriends += ["Ian", "Bomi", "Kevin"]
+
+//Q3
+myFriends.append("Michael")
+
+//Q4
+//insert(element, at:)
+myFriends.remove(at: 2)
+myFriends.insert("Kevin", at: 0)
+
+//Q5
+for friends in myFriends {
+    print(friends)
+}
+
+print("------")
+
+//Q6
+//myFriends[5] leads to a fatal error since the index is out of range.
+
+//Q7
+//Use the method first
+
+//Q8
+//Use the method last
+
+//Q9
+var myCountryNumber = [String: Int]()
+
+//Q10
+myCountryNumber = ["US":1, "GB":44, "JP":81]
+
+//Q11
+myCountryNumber.updateValue(0, forKey: "GB")
+
+//Q12
+//var emptyDictionary: [String: Int] = [:]
+
+//Q13
+//Use removeValue(forKey: "") OR Dict[ ] = nil
+
+// Control Flow
+//Q1
+let lottoNumbers = [10, 9, 8, 7, 6, 5]
+for i in lottoNumbers[3...5] {
+    print(i)
+}
+
+print("------")
+
+//Q2 (?)
+// The method randomElement can help
+var randomCount = Int.random(in: 1...6)
+
+for _ in 1...randomCount {
+    print(lottoNumbers.randomElement()!)
+}
+
+print("------")
+
+//Q3
+
+//Q4
+
+//Q5
+/*
+ While Loops check the condition before running the loop code, whereas Repeat While Loops run the loop code first.
+ Repeat-while is used if you want to guarantee the loop runs at least once before checking the condition.
+ */
+
+//Q6
+var isRaining = true
+if isRaining {
+    print("It's raining. I don't want to work today.")
+} else {
+    print("Although it's sunny, I still don't want to work today.")
+}
+
+//Q7
+var ourJob: [String: Int] = ["Member": 1, "Team Member": 2, "Manager": 3, "Director": 4]
+var jobLevel = 8
+
+switch jobLevel {
+case 1:
+    print("Member")
+case 2:
+    print("Team Member")
+case 3:
+    print("Manager")
+case 4:
+    print("Director")
+default:
+    print("We don't have this job.")
+}
+
+//Q1
+func greet(person: String) -> String {
+    return "Hello, " + person
+}
+
+//Q2
+func multiply(for a: Int, b: Int = 10) {
+    print(a*b)
+}
+
+//Q3
+//The argument label is used when calling the function. The parameter name is used in the implementation of the function.
+
+//Q4
+//String
+//Double
