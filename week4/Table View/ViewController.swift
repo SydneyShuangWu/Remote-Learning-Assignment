e58faa7ae36092a3e0d8b513098f4d5479f49af6
SydneyShuangWu <shuangwu0120@gmail.com>
2020-10-06 22:39:13 +0800

    Updated Week 4 Assignment

--- week4/Table View/ViewController.swift
+++ week4/Table View/ViewController.swift
@@ -13,10 +13,6 @@ class TableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // To set dynamic row height
-    table.rowHeight = UITableView.automaticDimension
-    table.estimatedRowHeight = 100
   }
 
   // To display 10 rows
@@ -28,29 +24,14 @@ class TableViewController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
     if let label = cell.viewWithTag(1000) as? UILabel {
-      if indexPath.row == 0 {
-        label.text = "This is section 0, row 0"
-      } else if indexPath.row == 1 {
-        label.text = "This is section 0, row 1"
-      } else if indexPath.row == 2 {
-        label.text = "This is section 0, row 2"
-      } else if indexPath.row == 3 {
-        label.text = "This is section 0, row 3"
-      } else if indexPath.row == 4 {
-        label.text = "This is section 0, row 4"
-      } else if indexPath.row == 5 {
-        label.text = "This is section 0, row 5"
-      } else if indexPath.row == 6 {
-        label.text = "This is section 0, row 6"
-      } else if indexPath.row == 7 {
-        label.text = "This is section 0, row 7"
-      } else if indexPath.row == 8 {
-        label.text = "This is section 0, row 8"
-      } else if indexPath.row == 9 {
-        label.text = "This is section 0, row 9"
-      }
+      label.text = "This is section \(indexPath.section), row \(indexPath.row)"
     }
     return cell
+  }
+  
+  // To set dynamic row height
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+     return 100
   }
 
   
